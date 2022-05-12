bdfff0a0ba6dd4e7b1a70726e9bfb8dba53492e9
onady <onady.dev@gmail.com>
2022-05-12 16:54:04 +0900

    프로그래머스-완전탐색-모의고사

--- sample.js
+++ sample.js
@@ -30,34 +30,47 @@ answers	    return
 
 모든 사람이 2문제씩을 맞췄습니다.
  */
-const log = console.log;
-const answers = [1, 4, 2, 2, 3, 4, 1, 2, 3, 4, 5, 5, 3, 1, 2];
+
 function solution(answers) {
   let answer = [];
-  let supoja1 = 0;
-  let supoja2 = 0;
-  let supoja3 = 0;
-  const supojaRule1 = [1, 2, 3, 4, 5];
-  const supojaRule2 = [2, 1, 2, 3, 2, 4, 2, 5];
-  const supojaRule3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+  let list = [
+    { name: 1, value: 0 },
+    { name: 2, value: 0 },
+    { name: 3, value: 0 }
+  ]
+  const supoja1 = [1, 2, 3, 4, 5];
+  const supoja2 = [2, 1, 2, 3, 2, 4, 2, 5];
+  const supoja3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
 
   for (const [index, value] of answers.entries()) {
-      if(value===supojaRule1[index%5]){
-        supoja1++
-      }
-      if(value===supojaRule2[index%8]){
-        supoja2++
-      }
-      if(value===supojaRule3[index%10]){
-        supoja3++
-      }
+    if (value === supoja1[index % supoja1.length]) {
+      list[0].value++
+    }
+    if (value === supoja2[index % supoja2.length]) {
+      list[1].value++
+    }
+    if (value === supoja3[index % supoja3.length]) {
+      list[2].value++
+    }
   }
-  log(supoja1, supoja2, supoja3)
-  
+
+  let sort = list.sort((a, b) => {
+    return b.value - a.value;
+  })
+
+  for (const [index, value] of sort.entries()) {
+    if (index === 0) {
+      answer.push(value.name)
+    } else if (value.value === sort[index - 1].value) {
+      answer.push(value.name)
+    } else {
+      break
+    }
+  }
+
   return answer;
 }
 
-console.clear();
-console.time("");
-console.log(solution(answers));
-console.timeEnd("");
+module.exports = {
+  solution: solution
+}
