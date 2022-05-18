9d8b181af1d9796e1a31200ac32ded3240ac4a23
onady <onady.dev@gmail.com>
2022-05-19 07:17:22 +0900

    15270친구팰린드롬

--- YungKwang(JS)/15270친구팰린드롬.js
+++ YungKwang(JS)/15270친구팰린드롬.js
@@ -1,23 +1,32 @@
 const fs = require("fs");
 const input = (process.platform === "linux" ?
     fs.readFileSync("/dev/stdin").toString() :
-    `4 4
+    `3 3
 1 2
 2 3
 3 1
-4 1`).trim().split("\n");
+`).trim().split("\n");
 
 const [N, M] = input.shift().split(" ").map(Number)
 const A = input.map(el => el.split(" ").map(Number))
 
-let result = new Set()
-
-for (const a of A) {
-    if (!result.has(a[0]) && !result.has(a[1])) {
-        result.add(a[0])
-        result.add(a[1])
+let max = 1
+for(let i=0; i<M; i++){
+    let set = new Set()
+    set.add(A[i][0])
+    set.add(A[i][1])
+    for(let k=1; k<M; k++){
+        if(!set.has(A[k][0]) && !set.has(A[k][1])){
+            set.add(A[k][0])
+            set.add(A[k][1])
+        }
+    }
+    console.log(set)
+    let size = set.size
+    if(size<N) size++
+    if(set.size > max){
+        max = size
     }
 }
 
-console.log(result, A)
-console.log(result.size < N ? result.size + 1 : result.size)
+console.log(max)
