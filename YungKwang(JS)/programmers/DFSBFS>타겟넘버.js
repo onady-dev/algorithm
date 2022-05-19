09254fcd0c943036284e88e9796d210a4cc346ec
onady <onady.dev@gmail.com>
2022-05-20 07:16:28 +0900

    programmers 타겟넘버

--- YungKwang(JS)/programmers/DFSBFS>타겟넘버.js
+++ YungKwang(JS)/programmers/DFSBFS>타겟넘버.js
@@ -1,6 +1,19 @@
 function solution(numbers, target) {
-    var answer = 0;
+    let answer = 0;
+    DFS(0,0)
+    const DFS = (depth, num) => {
+        if(depth === numbers.length){
+            if(num === target){
+                answer++
+            }
+            return;
+        }
+        // DFS(depth+1, num)
+    }
     return answer;
 }
 
 console.log(solution([1, 1, 1, 1, 1], 3))
+
+
+
