9736f7e2652328e4a38b0e9f3fa81c4d1b0ddc61
onady <onady.dev@gmail.com>
2022-05-20 16:17:24 +0900

    1065한수

--- YungKwang(JS)/programmers/DFSBFS>타겟넘버.js
+++ YungKwang(JS)/programmers/DFSBFS>타겟넘버.js
@@ -1,19 +1,19 @@
 function solution(numbers, target) {
     let answer = 0;
-    DFS(0,0)
-    const DFS = (depth, num) => {
-        if(depth === numbers.length){
-            if(num === target){
+    DFS(0, 0)
+    function DFS(depth, num) {
+        if (depth === numbers.length) {
+            if (num === target) {
                 answer++
             }
             return;
         }
-        // DFS(depth+1, num)
+        DFS(depth + 1, num + numbers[depth])
+        DFS(depth + 1, num - numbers[depth])
     }
     return answer;
 }
 
-console.log(solution([1, 1, 1, 1, 1], 3))
 
 
 
