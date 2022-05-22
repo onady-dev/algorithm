a2b1761dfb52c54bf3430c66c42db16973a7358a
onady <onady.dev@gmail.com>
2022-05-22 23:44:23 +0900

    programmers 해시>위장

---
+++ YungKwang(JS)/programmers/해시>위장.js
@@ -0,0 +1,19 @@
+function solution(clothes) {
+  
+
+  return clothes;
+}
+
+const clothes = [
+  // answer = 5
+  ["yellowhat", "headgear"],
+  ["blueglasses", "eyewear"],
+  ["green_turban", "headgear"],
+];
+// const clothes = [
+//     // answer = 3
+//     ["crowmask", "face"],
+//     ["bluesunglasses", "face"],
+//     ["smoky_makeup", "face"],
+//   ];
+console.log(solution(clothes));
