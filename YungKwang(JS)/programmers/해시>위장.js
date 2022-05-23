6f468fa73225f81bb7ca9d53ddb636a46a6275a0
onady <onady.dev@gmail.com>
2022-05-23 22:24:08 +0900

    해시>위장

--- YungKwang(JS)/programmers/해시>위장.js
+++ YungKwang(JS)/programmers/해시>위장.js
@@ -1,8 +1,23 @@
 function solution(clothes) {
-  
-
-  return clothes;
+  let type = new Set();
+  let name = new Set();
+  let answer = 0
+  search(clothes)
+  function search(arr){
+    arr.forEach((value, index, arr) => {
+      if (!name.has(value[0]) && !type.has(value[1])) {
+        name.add(value[0]);
+        type.add(value[1]);
+        answer++
+        search(arr.slice(index + 1))
+      }
+      type.clear()
+      name.clear()
+    });
+  }
+  return answer;
 }
+
 
 const clothes = [
   // answer = 5
