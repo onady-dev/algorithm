aca87798c536a315f666089d482c545fa7297d6d
onady <onady.dev@gmail.com>
2022-05-31 07:11:37 +0900

    시리얼번호

--- YungKwang(JS)/BaekJoon/1431시리얼번호.js
+++ YungKwang(JS)/BaekJoon/1431시리얼번호.js
@@ -1,4 +1,19 @@
 const fs = require("fs");
 const input = (process.platform === "linux" ?
     fs.readFileSync("/dev/stdin").toString() :
-    ``).trim().split("\n");
+    `5
+ABCD
+145C
+A
+A910
+Z321`).trim().split("\n");
+
+const N = input.shift()
+const arrays = input.sort((a, b) => a.length - b.length)
+
+const regex = /[^0-9]/g;
+arrays.map(v => {
+    console.log("@@" , v.replace(regex, ""))
+})
+
+
