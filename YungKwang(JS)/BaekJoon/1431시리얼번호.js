f5e426af2e8f5a52a27206dc93c4d5e39eb08469
onady <onady.dev@gmail.com>
2022-05-31 14:40:32 +0900

    시리얼번호

--- YungKwang(JS)/BaekJoon/1431시리얼번호.js
+++ YungKwang(JS)/BaekJoon/1431시리얼번호.js
@@ -4,13 +4,24 @@ const input = (process.platform === "linux" ?
     `5
 ABCDE
 BCDEF
-ABCDA
-BAAAA
+C12DA
+B12AA
 ACAAA`).trim().split("\n");
 
 const N = input.shift()
 const arrays = input.sort()
-arrays.sort((a, b) => a.length - b.length)
+
+console.log(arrays, 1)
+
+arrays.sort((a, b) => {
+    return a.length - b.length
+})
+
+console.log(arrays, 2)
+
+// arrays.sort((a, b) => {
+//     if (num(a) !== -1 && num(b) !== -1) return num(a) - num(b)
+// })
 arrays.sort((a, b) => num(a) - num(b))
 
 function num(str) {
@@ -20,4 +31,17 @@ function num(str) {
     return arr.reduce((a, b) => a + b)
 }
 
-console.log(arrays)
+console.log(arrays, 3)
+
+// const dap = ['FIPJOTEA5',
+//     'PYF1J14TF',
+//     '34H2BJS6N',
+//     'PIM12MD7RCOLWW09']
+const dap = [
+    'A',
+    'ABCD',
+    'Z321',
+    '145C',
+    'A910'
+]
+console.log(dap)
