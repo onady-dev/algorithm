f284fce40346eeceaf0ce6b0ab964ffe0e7684a6
onady <onady.dev@gmail.com>
2022-06-08 16:43:27 +0900

    연결요소의개수

--- YungKwang(JS)/BaekJoon/11724연결요소의개수.js
+++ YungKwang(JS)/BaekJoon/11724연결요소의개수.js
@@ -1,10 +1,19 @@
 const fs = require("fs");
 // const input = fs.readFileSync("/dev/stdin").toString().trim().split('\n')
 const input = `
-6 5
+5 4
 1 2
 2 5
 5 1
 3 4
-4 6
 `.trim().split('\n')
+
+const [N, M] = input.shift().split(" ").map(Number)
+const uv = input.map(v => v.split(" ").map(Number))
+let graph = Array.from(Array(N + 1), () => Array(N + 1).fill(0));
+
+uv.map((v, i) => {
+    graph[v[0]][v[1]] = 1
+    graph[v[1]][v[0]] = 1
+})
+console.log(graph)
