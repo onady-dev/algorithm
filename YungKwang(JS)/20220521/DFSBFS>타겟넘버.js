c453ef4b9865f4fb6e44f489646e135bff6b8ac8
onady <onady.dev@gmail.com>
2022-06-17 16:26:54 +0900

    복습

--- YungKwang(JS)/20220521/DFSBFS>타겟넘버.js
+++ YungKwang(JS)/20220521/DFSBFS>타겟넘버.js
@@ -1,3 +1,25 @@
+/* 
+    20220617
+*/
+function solution(numbers, target) {
+    let answer = 0
+    DFS(0, 0)
+    function DFS(num, depth) {
+        if (depth === numbers.length) {
+            if (num === target) {
+                answer++
+            }
+            return
+        }
+        DFS(num + numbers[depth], depth + 1)
+        DFS(num - numbers[depth], depth + 1)
+    }
+    return answer
+}
+console.log(solution([4, 1, 2, 1], 4))
+
+
+/* 
 function solution(numbers, target) {
     let answer = 0;
     DFS(0, 0)
@@ -13,7 +35,7 @@ function solution(numbers, target) {
     }
     return answer;
 }
-
+*/
 
 
 
