53a7e000f4e1a190738961010617b78d5cba4dc6
onady <onady.dev@gmail.com>
2022-06-24 13:22:24 +0900

    계단오르기

--- YungKwang(JS)/20220625/2579계단오르기.js
+++ YungKwang(JS)/20220625/2579계단오르기.js
@@ -1,35 +1,45 @@
 const fs = require("fs");
 // const input = fs.readFileSync("/dev/stdin").toString().trim().split('\n')
 const input = `
-100 90
+6
+10
+20
+15
+25
+10
+20
 `.trim().split('\n')
 
-const [X, Y] = input.shift().split(" ").map(Number)
-const Z = Math.floor((Y * 100 / X))
+const N = +input.shift()
+const arr = input.map(Number)
 
-// let count = 0
-// while (true) {
-//     if (Z === 100 || Z === 99) {
-//         count = -1
-//         break
+const dp = new Array(N).fill(0);
+
+dp[0] = arr[0];
+dp[1] = arr[0] + arr[1];
+dp[2] = Math.max(arr[0] + arr[2], arr[1] + arr[2]);
+
+for (let i = 3; i < N; i++) {
+    console.log(arr[i] + arr[i - 1] + dp[i - 3], arr[i] + dp[i - 2])
+    dp[i] = Math.max(arr[i] + arr[i - 1] + dp[i - 3], arr[i] + dp[i - 2]);
+}
+
+console.log(dp);
+
+// let answer = 0
+// DFS(0, 0, 1)
+// DFS(1, 0, 1)
+// function DFS(stair, score, check) {
+//     check++
+//     score += stairs[stair]
+//     if (N - 2 === stair && check < 3) DFS(stair + 1, score, check)
+//     if (N - 2 > stair) {
+//         if (check < 3) DFS(stair + 1, score, check)
+//         DFS(stair + 2, score, 1)
 //     }
-//     count++
-//     if (Math.floor((Y + count) * 100 / (X + count)) !== Z) break
+//     console.log(stair, score, check)
+//     if (answer < score && N - 1 === stair) answer = score
+
 // }
 
-// console.log(count)
-
-function binarysearch(first, last) {
-    const mid = Math.floor((first + last) / 2)
-    if (first >= last) return mid
-    const temp = Math.floor(((Y + mid) * 100 / (X + mid)))
-    if (temp > Z) return binarysearch(first, mid)
-    if (temp <= Z) return binarysearch(mid + 1, last)
-}
-
-let result = 0
-result = binarysearch(0, 100000000001)
-if (result === 100000000001) {
-    result = -1
-}
-console.log(result)
+// console.log(answer)
