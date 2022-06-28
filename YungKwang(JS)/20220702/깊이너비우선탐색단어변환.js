5812e43462babd2e21547462487fe941f02c91d5
onady <onady.dev@gmail.com>
2022-06-28 16:41:49 +0900

    단어변환

--- YungKwang(JS)/20220702/깊이너비우선탐색단어변환.js
+++ YungKwang(JS)/20220702/깊이너비우선탐색단어변환.js
@@ -1,17 +1,25 @@
 function solution(begin, target, words) {
     var answer = 0;
-    var visited = []
-    function DFS(word, i) {
-        var chk = 0
-        var alpahbet = word.split("")
-        words[i].split("").forEach(v => {
-            if (alpahbet.include(v)) chk++
+    var visited = [];
+    var queue = [];
+    if (!words.includes(target)) return 0;
+    queue.push([begin, answer])
+    while (queue) {
+        var [word, count] = queue.shift();
+        if (word === target) return count
+        words.forEach(v => {
+            var check = 0
+            if (visited.includes(v)) return;
+            for (var i = 0; i < v.length; i++) {
+                if (v[i] !== word[i]) check++
+            }
+            if (check === 1) {
+                queue.push([v, ++count])
+                visited.push(v)
+            }
         });
-        if (chk === 1) DFS(words[i], i + 1)
     }
-
     return answer;
 }
 
-solution("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"])
-console.log("word".split(""))
+console.log(solution("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"]))
