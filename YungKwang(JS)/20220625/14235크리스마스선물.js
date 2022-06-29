1dded4062187dd853a05482f984ca41a2c25abb1
onady <onady.dev@gmail.com>
2022-06-29 11:17:27 +0900

    대칭차집합

--- YungKwang(JS)/20220625/14235크리스마스선물.js
+++ YungKwang(JS)/20220625/14235크리스마스선물.js
@@ -1,32 +1,16 @@
 const fs = require("fs");
 // const input = fs.readFileSync("/dev/stdin").toString().trim().split('\n')
 const input = `
-<<<<<<< HEAD
-8
-2 1 1
-0
-0
-3 2 1 1
-0
-0
-3 3 2 2
-0
-=======
 5
 0
 2 3 2
 0
 0
 0
->>>>>>> a8fb88d62ff879a5fb00ab0dcc43f7ff9276bf83
 `.trim().split('\n')
 
 const N = +input.shift()
 const A = input.map(v => v == 0 ? Number(v) : v.split(" ").map(Number))
-<<<<<<< HEAD
-=======
-
->>>>>>> a8fb88d62ff879a5fb00ab0dcc43f7ff9276bf83
 let gift = []
 A.forEach((v) => {
     if (v === 0) {
@@ -36,7 +20,4 @@ A.forEach((v) => {
         gift.sort((a, b) => b - a)
     }
 });
-<<<<<<< HEAD
 console.log(gift)
-=======
->>>>>>> a8fb88d62ff879a5fb00ab0dcc43f7ff9276bf83
--- YungKwang(JS)/20220625/2579계단오르기.js
+++ YungKwang(JS)/20220625/2579계단오르기.js
@@ -11,40 +11,6 @@ const input = `
 `.trim().split('\n')
 
 const N = +input.shift()
-<<<<<<< HEAD
-const arr = input.map(Number)
-
-const dp = new Array(N).fill(0);
-
-dp[0] = arr[0];
-dp[1] = arr[0] + arr[1];
-dp[2] = Math.max(arr[0] + arr[2], arr[1] + arr[2]);
-
-for (let i = 3; i < N; i++) {
-    console.log(arr[i] + arr[i - 1] + dp[i - 3], arr[i] + dp[i - 2])
-    dp[i] = Math.max(arr[i] + arr[i - 1] + dp[i - 3], arr[i] + dp[i - 2]);
-}
-
-console.log(dp);
-
-// let answer = 0
-// DFS(0, 0, 1)
-// DFS(1, 0, 1)
-// function DFS(stair, score, check) {
-//     check++
-//     score += stairs[stair]
-//     if (N - 2 === stair && check < 3) DFS(stair + 1, score, check)
-//     if (N - 2 > stair) {
-//         if (check < 3) DFS(stair + 1, score, check)
-//         DFS(stair + 2, score, 1)
-//     }
-//     console.log(stair, score, check)
-//     if (answer < score && N - 1 === stair) answer = score
-
-// }
-
-// console.log(answer)
-=======
 const stairs = input.map(Number)
 
 let answer = 0
@@ -63,4 +29,3 @@ function DFS(stair, score, check) {
 }
 
 console.log(answer)
->>>>>>> a8fb88d62ff879a5fb00ab0dcc43f7ff9276bf83
